65ada55fb72189178151a347dfe8ed530749e460
Giovani Andrade <giovani.andrade@triersistemas.com.br>
2025-07-16 23:29:22 -0300

    Lidando com erros de timeout

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/api/controller/SensorMonitoringController.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/api/controller/SensorMonitoringController.java
@@ -6,13 +6,18 @@ import com.algaworks.algasensors.temperature.monitoring.domain.model.SensorMonit
 import com.algaworks.algasensors.temperature.monitoring.domain.repository.SensorMonitoringRepository;
 import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/monitoring")
@@ -32,18 +37,30 @@ public class SensorMonitoringController {
                 .build();
     }
 
-    @PutMapping("/enabled")
+    @PutMapping("/enable")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void enable(@PathVariable TSID sensorId) {
         SensorMonitoring sensorMonitoring = findByIdOrDefault(sensorId);
+
+        if (sensorMonitoring.getEnabled()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
         sensorMonitoring.setEnabled(true);
         sensorMonitoringRepository.saveAndFlush(sensorMonitoring);
     }
 
-    @PutMapping("/disabled")
+    // Não gostei muito dessa abordagem
+    @DeleteMapping("/enable")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void disabled(@PathVariable TSID sensorId) {
+    @SneakyThrows // Isso é necessário para o Thread.sleep funcionar sem precisar declarar a exceção
+    public void disable(@PathVariable TSID sensorId) {
         SensorMonitoring sensorMonitoring = findByIdOrDefault(sensorId);
+
+        if (!sensorMonitoring.getEnabled()) {
+            Thread.sleep(Duration.ofSeconds(10));
+        }
+
         sensorMonitoring.setEnabled(false);
         sensorMonitoringRepository.saveAndFlush(sensorMonitoring);
     }
